a4718a13ab62d9387fa86365601071be1f298971
pignaciofranco <franco.p.ignacio@gmail.com>
2021-03-11 02:42:10 -0300

    corregir

--- app.js
+++ app.js
@@ -11,25 +11,24 @@ const puerto = process.env.PORT ? process.env.PORT : 3000;
 
 app.use = (express.json());
 app.use (express.urlencoded()); // decodifica la url, recibe desde el cliente
-app.use (express.json()); // mapeo de json a obj js
 app.use (express.static('static')); // para manejar archivos estaticos
 
-const conexion = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bdtp'
-});
-
-conexion.connect( (error) => {
-     if (error) {
-        throw error;
-    }
-    console.log ('Conexión a la BD establecida!');
-});
-
-// Para pasar de callback a async-await
-const qy = util.promisify(conexion.query).bind(conexion);
+//const conexion = mysql.createConnection ({
+//    host: 'localhost',
+//    user: 'root',
+//    password: '',
+//    database: 'bdtp'
+//});
+//
+//conexion.connect( (error) => {
+//     if (error) {
+//        throw error;
+//    }
+//    console.log ('Conexión a la BD establecida!');
+//});
+//
+//// Para pasar de callback a async-await
+//const qy = util.promisify(conexion.query).bind(conexion);
 
 /**
  * Base de datos. Registro de alumnos:
@@ -43,81 +42,83 @@ const qy = util.promisify(conexion.query).bind(conexion);
   * Apellido: apellido del alumno, varchar 50 (obligatorio)
   * dni: dni del alumno, int 8 (obligatorio) 
   * constraseña: varchar codificada 100 (obligatorio)
-  */
+*/
+//
+//// post para registrar un alumno
+//app.post ('/api/registro', async (req, res) => {
+//    try {
+//        // verifico que se envio un nombre, apellido y dni
+//        if (!req.body.nombre || !req.body.apellido || !req.body.dni || !req.body.clave) {
+//            throw new Error ('Faltaron datos');
+//        }
+//
+//        // verifico que no exista un alumno registrado
+//        let query = 'SELECT * FROM alumnos WHERE (nombre = ? AND apellido = ?) OR dni = ?';
+//        let respuesta = await qy (query, [req.body.nombre, req.body.apellido, req.body.dni]);
+//
+//        if (respuesta.length > 0)
+//            throw new Error ('Ya se registro el alumno');
+//        
+//        // se termino la verificación
+//
+//        // ingreso alumno
+//        // codifico la contraseña
+//        const claveEncriptada = await bcrypt.hash (req.body.clave, 10);
+//
+//        // guardo los datos
+//        const alumno = {
+//            nombre: req.body.nombre,
+//            apellido: req.body.apellido,
+//            dni: req.body.dni,
+//            clave: claveEncriptada // falta codificar
+//        };
+//        // inserto los datos en la BD
+//        query = 'INSERT INTO alumnos (nombre, apellido, dni, clave) VALUE (?,?,?,?)';
+//        respuesta = await qy (query, [alumno.nombre, alumno.apellido, alumno.dni, alumno.clave]);
+//        console.log(respuesta);
+//        res.json (respuesta);
+//
+//        // respuesta.insertId , busca el alumno ingresado a la BD por medio del inserId que retorna BD indicando que 
+//        // se realizo el ingreso correctame
+//        const registroInsertado = await qy('select * from persona where id=?', [respuesta.insertId]);
+//        res.json(registroInsertado[0]);
+//
+//    }
+//    catch (error) {
+//        console.log(error.message);
+//        res.status(413).send ({"error":error.message});
+//    }
+//});
 
 // get devuelve la lista de alumnos
-app.get ('/api/alumnos', async (req, res) => {
-    try {
-        let query = 'SELECT * FROM alumnos';
-        let respuesta = await qy (query); 
-        //res.send ({"respuesta":respuesta});
-        res.json (respuesta);
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(413).send.json (error.message);
-    }
-});
+//app.get ('/api/alumnos', async (req, res) => {
+//    try {
+//        let query = 'SELECT nombre, apellido, dni FROM alumnos';
+//        let respuesta = await qy (query); 
+//        res.send ({"respuesta":respuesta});
+//        //res.json (respuesta); 
+//    }
+//    catch (error) {
+//        console.log(error.message);
+//        res.status(413).send.json (error.message);
+//    }
+//});
 
-// post para registrar un alumno
-app.post ('/api/registro', async (req, res) => {
-    try {
-        // verifico que se envio un nombre, apellido y dni
-        if (!req.body.nombre || !req.body.apellido || !req.body.dni || !req.body.clave) {
-            throw new Error ('Faltaron datos');
-        }
-
-        // verifico que no exista un alumno registrado
-        let query = 'SELECT * FROM alumnos WHERE (nombre = ? AND apellido = ?) OR dni = ?';
-        let respuesta = await qy (query, [req.body.nombre, req.body.apellido, req.body.dni]);
-
-        if (respuesta.length > 0)
-            throw new Error ('Ya se registro el alumno');
-        
-        // se termino la verificación
-
-        // ingreso alumno
-        // codifico la contraseña
-        const claveEncriptada = await bcrypt.hash (req.body.clave, 10);
-
-        // guardo los datos
-        const alumno = {
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            dni: req.body.dni,
-            clave: claveEncriptada // falta codificar
-        };
-
-        query = 'INSERT INTO alumnos (nombre, apellido, dni, clave) VALUE (?,?,?,?)';
-        respuesta = await qy (query, [alumno.nombre, alumno.apellido, alumno.dni, alumno.clave]);
-        console.log(respuesta);
-        //res.json (respuesta);
-
-        // respuesta.insertId
-        const registroInsertado = await query('select * from persona where id=?', [respuesta.insertId]);
-        res.json(registroInsertado[0]);
-
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(413).send ({"error":error.message});
-    }
-})
 
 // PUT actualiza un registro
-app.put ('/api/alumnos/:id', async (req,res) => {
-    try {
-        const id = req.params.id;
-        let consulta = 'SELECT * FROM alumnos WHERE id = ?';
-        let respuesta = await qy ( consulta, [id] );
-        if (consulta.length > 0)
-            throw new Error ('No existe el alumno');
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(413).send ({"error":error.message});
-    }
-});
+//app.put ('/api/alumnos/:id', async (req,res) => {
+//    try {
+//        const id = req.params.id;
+//        let consulta = 'SELECT * FROM alumnos WHERE id = ?';
+//        let respuesta = await qy ( consulta, [id] );
+//        if (consulta.length > 0)
+//            throw new Error ('No existe el alumno');
+//    }
+//    catch (error) {
+//        console.log(error.message);
+//        res.status(413).send ({"error":error.message});
+//    }
+//});
 
 // Servidor 
 app.listen (puerto, () => {
